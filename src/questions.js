0f62733b8c68aa817222d71e9bb2ba08e8117000
ANAGHA R <anagharanjith2@gmail.com>
2024-03-10 21:53:03 +0530

    que added

--- src/questions.js
+++ src/questions.js
@@ -89,115 +89,115 @@ export const data = [
         ],
         correctOption: 1,
     },
-    // {
-    //     question: "Who was the 'Father of the Indian Constitution'?",
-    //     options: [
-    //         "Jawaharlal Nehru",
-    //         "B.R. Ambedkar",
-    //         "Sardar Vallabhbhai Patel",
-    //         "Mahatma Gandhi"
-    //     ],
-    //     correctOption: 1,
-    // },
-    // {
-    //     question: "Which Indian state is known as the 'Land of White Elephants'?",
-    //     options: [
-    //         "Kerala",
-    //         "Assam",
-    //         "West Bengal",
-    //         "Manipur"
-    //     ],
-    //     correctOption: 1,
-    // },
-    // {
-    //     question: "Who is known as the 'Iron Man of India'?",
-    //     options: [
-    //         "Jawaharlal Nehru",
-    //         "Sardar Vallabhbhai Patel",
-    //         "Subhas Chandra Bose",
-    //         "Indira Gandhi"
-    //     ],
-    //     correctOption: 1,
-    // },
-    // {
-    //     question: "Which Indian city is famous for its annual Kumbh Mela?",
-    //     options: [
-    //         "Haridwar",
-    //         "Varanasi",
-    //         "Allahabad (Prayagraj)",
-    //         "Ujjain"
-    //     ],
-    //     correctOption: 2,
-    // },
-    // {
-    //     question: "Which Indian state is known for its famous backwaters?",
-    //     options: [
-    //         "Goa",
-    //         "Kerala",
-    //         "Tamil Nadu",
-    //         "Andhra Pradesh"
-    //     ],
-    //     correctOption: 1,
-    // },
-    // {
-    //     question: "What is the national flower of India?",
-    //     options: [
-    //         "Lotus",
-    //         "Rose",
-    //         "Marigold",
-    //         "Jasmine"
-    //     ],
-    //     correctOption: 0,
-    // },
-    // {
-    //     question: "Which Indian physicist was awarded the Nobel Prize in Physics for his work on the quantum theory?",
-    //     options: [
-    //         "C.V. Raman",
-    //         "Srinivasa Ramanujan",
-    //         "Homi J. Bhabha",
-    //         "Subrahmanyan Chandrasekhar"
-    //     ],
-    //     correctOption: 0,
-    // },
-    // {
-    //     question: "Who is known as the 'Nightingale of India'?",
-    //     options: [
-    //         "Sarojini Naidu",
-    //         "Indira Gandhi",
-    //         "Kalpana Chawla",
-    //         "Mother Teresa"
-    //     ],
-    //     correctOption: 0,
-    // },
-    // {
-    //     question: "What is the highest civilian award in India?",
-    //     options: [
-    //         "Padma Vibhushan",
-    //         "Bharat Ratna",
-    //         "Padma Bhushan",
-    //         "Padma Shri"
-    //     ],
-    //     correctOption: 1,
-    // },
-    // {
-    //     question: "Which Indian city is home to the Indian Institute of Technology (IIT) Bombay?",
-    //     options: [
-    //         "Bangalore",
-    //         "Mumbai",
-    //         "Chennai",
-    //         "Kolkata"
-    //     ],
-    //     correctOption: 1,
-    // },
-    // {
-    //     question: "Who was the first woman Prime Minister of India?",
-    //     options: [
-    //         "Indira Gandhi",
-    //         "Sonia Gandhi",
-    //         "Mayawati",
-    //         "Mamata Banerjee"
-    //     ],
-    //     correctOption: 0,
-    // }
+    {
+        question: "Who was the 'Father of the Indian Constitution'?",
+        options: [
+            "Jawaharlal Nehru",
+            "B.R. Ambedkar",
+            "Sardar Vallabhbhai Patel",
+            "Mahatma Gandhi"
+        ],
+        correctOption: 1,
+    },
+    {
+        question: "Which Indian state is known as the 'Land of White Elephants'?",
+        options: [
+            "Kerala",
+            "Assam",
+            "West Bengal",
+            "Manipur"
+        ],
+        correctOption: 1,
+    },
+    {
+        question: "Who is known as the 'Iron Man of India'?",
+        options: [
+            "Jawaharlal Nehru",
+            "Sardar Vallabhbhai Patel",
+            "Subhas Chandra Bose",
+            "Indira Gandhi"
+        ],
+        correctOption: 1,
+    },
+    {
+        question: "Which Indian city is famous for its annual Kumbh Mela?",
+        options: [
+            "Haridwar",
+            "Varanasi",
+            "Allahabad (Prayagraj)",
+            "Ujjain"
+        ],
+        correctOption: 2,
+    },
+    {
+        question: "Which Indian state is known for its famous backwaters?",
+        options: [
+            "Goa",
+            "Kerala",
+            "Tamil Nadu",
+            "Andhra Pradesh"
+        ],
+        correctOption: 1,
+    },
+    {
+        question: "What is the national flower of India?",
+        options: [
+            "Lotus",
+            "Rose",
+            "Marigold",
+            "Jasmine"
+        ],
+        correctOption: 0,
+    },
+    {
+        question: "Which Indian physicist was awarded the Nobel Prize in Physics for his work on the quantum theory?",
+        options: [
+            "C.V. Raman",
+            "Srinivasa Ramanujan",
+            "Homi J. Bhabha",
+            "Subrahmanyan Chandrasekhar"
+        ],
+        correctOption: 0,
+    },
+    {
+        question: "Who is known as the 'Nightingale of India'?",
+        options: [
+            "Sarojini Naidu",
+            "Indira Gandhi",
+            "Kalpana Chawla",
+            "Mother Teresa"
+        ],
+        correctOption: 0,
+    },
+    {
+        question: "What is the highest civilian award in India?",
+        options: [
+            "Padma Vibhushan",
+            "Bharat Ratna",
+            "Padma Bhushan",
+            "Padma Shri"
+        ],
+        correctOption: 1,
+    },
+    {
+        question: "Which Indian city is home to the Indian Institute of Technology (IIT) Bombay?",
+        options: [
+            "Bangalore",
+            "Mumbai",
+            "Chennai",
+            "Kolkata"
+        ],
+        correctOption: 1,
+    },
+    {
+        question: "Who was the first woman Prime Minister of India?",
+        options: [
+            "Indira Gandhi",
+            "Sonia Gandhi",
+            "Mayawati",
+            "Mamata Banerjee"
+        ],
+        correctOption: 0,
+    }
     
 ];
